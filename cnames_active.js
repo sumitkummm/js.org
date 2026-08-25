dea8e16a53d53cf7ffb8fb82926d911e40d1a8b5
sumitkummm <mk298997@gmail.com>
2026-08-25 10:25:46 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3207,6 +3207,7 @@ var cnames_active = {
   "sulky": "shingle.github.io/sulky", // noCF? (don´t add this in a new PR)
   "sum": "ultirequiem.github.io/sum",
   "suman": "devsumanmdn.github.io",
+  "sumitkumawat":"prosumitkumawat.github.io"
   "summer": "minecraftpublisher.github.io/SummerJS",
   "sunar": "cname.vercel-dns.com", // noCF
   "sunos": "sunos22.github.io",
